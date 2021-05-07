1fdb9d9a6964d1225d9d97fa6ab720a390cc2dc7
JeonBest <JeonBest@github.com>
2021-05-08 01:03:09 +0900

    leet code num\#720

--- Trie/720.py
+++ Trie/720.py
@@ -1,35 +1,46 @@
 # LeetCode
 # 720. Longest Word in Dictionary
 
-class Solution:
-
-    class Node:
-        def __init__(self, key, count=0):
-            self.key = key
-            self.child = {}
-
-    class Trie:
-        def __init__(self):
-            self.head = Node(None)
-        
-        def insert(word):
-            curNode = self.head
-
-            for letter in word:
-                if letter not in curNode.child:
-                    curNode.child [letter] = Node(letter)
+class Node:
+    def __init__(self, key, count=0):
+        self.key = key
+        self.child = {}
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+    
+    def insert(self, word):
+        curNode = self.head
+        for letter in word:
+            if letter not in curNode.child:
+                curNode.child [letter] = Node(letter)
+            curNode = curNode.child[letter]
+        curNode.child['*'] = True
+    
+    def searchValid(self, word):
+        curNode = self.head
+        isValid = True
+        for letter in word:
+            if curNode != self.head and '*' not in curNode.child:
+                isValid = False
+            if letter in curNode.child:
                 curNode = curNode.child[letter]
-            curNode.child['*'] = True
-        
-        def search(word):
-            curNode = self.head
+        if '*' in curNode.child and isValid:
+            return True
+        else:
+            return False
 
-            for letter in word:
-                if letter in curNode.child:
-                    curNode = curNode.child[letter]
-            if '*' in curNode.child:
-                return True
-            else:
-                return False
-
+class Solution:
     def longestWord(self, words: List[str]) -> str:
+        wordTrie = Trie()
+        for word in words:
+            wordTrie.insert(word)
+        
+        validwords = ""
+        for word in words:
+            if wordTrie.searchValid(word):
+                # same length => lexicographical order
+                if (len(word) == len(validwords) and word < validwords) or len(word) > len(validwords):
+                    validwords = word
+        
+        return validwords
