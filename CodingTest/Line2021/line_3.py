cdc9bf850984d8c0b6ef90d21d45a4abc555f25e
JeonBest <jelly9604@naver.com>
2021-11-27 03:22:55 +0900

    move to folder

---
+++ CodingTest/Line2021/line_3.py
@@ -0,0 +1,17 @@
+def solution(ads):    
+    answer = 0
+    
+    # greedy로 풀면 될듯?
+    
+    return answer
+
+def assertCode(answer, submit):
+    print("\t", "----- PASS -----" if answer == submit else "----- FAIL -----",
+          "\nExpect:", answer, "\tResult:", submit)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    
